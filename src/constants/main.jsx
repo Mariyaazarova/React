859d072eeaf47432af11bbcef1cac91cdd2b65c5
Maria <mariyaazarova@inbox.ru>
2024-12-20 18:13:56 +0300

    feat: добавить hw-3

---
+++ src/constants/main.jsx
@@ -0,0 +1,6 @@
+import { createRoot } from "react-dom/client";
+import { App } from "./app";
+
+const root = document.getElementById("root");
+const reactRoot = createRoot(root);
+reactRoot.render(<App />);
