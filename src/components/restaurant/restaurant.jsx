3b615a456fcf161cab4213060edd0cd996799917
Maria <mariyaazarova@inbox.ru>
2024-12-31 02:54:38 +0300

    feat: добавить hw-6

--- src/components/restaurant/restaurant.jsx
+++ src/components/restaurant/restaurant.jsx
@@ -1,14 +1,21 @@
+import { useAuth } from "../auth-context/use-auth";
 import { Container } from "../container/container";
 import { RestaurantMenu } from "../restaurant-menu/restaurant-menu";
 import { RestaurantReviews } from "../restaurant-reviews/restaurant-reviews";
 
 export const Restaurant = ({ restaurant }) => {
   const { name } = restaurant;
+  const { auth } = useAuth();
+
+  if (!name) {
+    return null;
+  }
+
   return (
     <Container>
       <h2>{name}</h2>
       <RestaurantMenu restaurant={restaurant} />
-      <RestaurantReviews restaurant={restaurant} />
+      {auth.isAuthorized && <RestaurantReviews restaurant={restaurant} />}
     </Container>
   );
 };
