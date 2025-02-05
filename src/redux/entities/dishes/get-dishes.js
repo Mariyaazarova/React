28ad84196eff7c7c690df9b20a8e3d31236b55d8
dmitry-vs <dmitryselin@inbox.ru>
2025-02-05 22:21:45 +0300

    feat: добавить условную загрузку для блюд отдельных ресторанов (за рамками ДЗ)

--- src/redux/entities/dishes/get-dishes.js
+++ src/redux/entities/dishes/get-dishes.js
@@ -1,5 +1,7 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { API_BASE_URL } from "../../consts";
+import { selectRestaurantById } from "../restaurants/restaurants-slice";
+import { selectDishesByIds } from "./dishes-slice";
 
 export const getDishes = createAsyncThunk(
   "dishes/fetchDishes",
@@ -14,5 +16,29 @@ export const getDishes = createAsyncThunk(
       return;
     }
     return result;
+  },
+  {
+    condition: (restaurantId, { getState }) => {
+      const state = getState();
+      // если нужные блюда есть в сторе, то не запрашивать
+      // нужные - значит, блюда данного ресторана
+      // нужно получить ресторан из стора (если его нет, отправляем запрос)
+      const restaurant = selectRestaurantById(state, restaurantId);
+      const restaurantDishesIds = restaurant.menu;
+      if (!restaurant || !Array.isArray(restaurantDishesIds)) return true;
+
+      // нужно получить блюда этого ресторана из стора
+      const dishes = selectDishesByIds(state, restaurantDishesIds);
+      const dishesIds = dishes.map((item) => item.id);
+
+      // если все блюда ресторана присутствуют в dishes, не отправляем запрос
+      if (restaurantDishesIds.every((item) => dishesIds.includes(item))) {
+        return false;
+      }
+      // иначе, отправляем запрос
+      else {
+        return true;
+      }
+    },
   }
 );
